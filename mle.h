34c28dfdbce6e7392107c55927d266dfb2a786fd
Adam Saponara <as@php.net>
2018-08-11 13:48:45 -0400

    bump to version v1.2

--- mle.h
+++ mle.h
@@ -576,7 +576,7 @@ void str_append_replace_with_backrefs(str_t* str, char* subj, char* repl, int pc
 extern editor_t _editor;
 
 // Macros
-#define MLE_VERSION "1.2-dev"
+#define MLE_VERSION "1.2"
 
 #define MLE_OK 0
 #define MLE_ERR 1
