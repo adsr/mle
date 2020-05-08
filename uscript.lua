3f3b9175fe3e124d2f06ca9bee2346a8fb7c5758
Adam Saponara <as@php.net>
2020-05-08 17:56:08 -0400

    update uscript.lua

--- uscript.lua
+++ uscript.lua
@@ -1,4 +1,4 @@
--- mle -N -x uscript.lua -K lua_kmap,,1 -k cmd_lua_test,F11,
+-- mle -N -x uscript.lua -K lua_kmap,,1 -k cmd_lua_test,F11, -n lua_kmap
 
 mle.editor_register_observer("buffer:save", function (bview)
     r = mle.util_shell_exec("ls", -1)
@@ -12,4 +12,14 @@ mle.editor_register_cmd("cmd_lua_test", function (ctx)
     else
         print("you hit cancel")
     end
+end)
+
+mle.editor_register_observer("cmd:cmd_copy:before", function (ctx)
+    local rv = mle.cursor_get_anchor(ctx["cursor"])
+    local anchor = rv["ret_anchor"]
+
+    rv = mle.mark_get_between_mark(ctx["mark"], anchor)
+    rv = mle.util_escape_shell_arg(rv["ret_str"])
+
+    mle.util_shell_exec("echo -n " .. rv["output"] .. " | xclip -sel c", 0)
 end)
