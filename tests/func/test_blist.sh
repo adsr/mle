2996faf0daf9f0d9ef3175ac973719dcb01713c5
Adam Saponara <as@php.net>
2023-04-15 01:00:53 -0400

    add test for cmd_last and cmd_goto

--- tests/func/test_blist.sh
+++ tests/func/test_blist.sh
@@ -7,3 +7,11 @@ expected[bview_count   ]='^bview_count=2'
 expected[bview_file42  ]='^bview.[[:digit:]]+.buffer.path=file42$'
 expected[bview_headless]='^bview.[[:digit:]]+.buffer.path=$'
 source "test.sh"
+
+extra_opts=(apple banana carrot)
+macro='M-2 M-3 M-0 M-c M-1 M-c'
+declare -A expected
+expected[carrot_count   ]='^bview_count=2'
+expected[carrot_file    ]='^bview.[[:digit:]]+.buffer.path=carrot$'
+expected[carrot_headless]='^bview.[[:digit:]]+.buffer.path=$'
+source "test.sh"
