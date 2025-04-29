fe23e41d80e42548c99304984894502969eaa4f0
Adam Saponara <as@php.net>
2025-04-28 23:39:41 -0400

    escape browse path in `cmd_browse`
    previously, if a directory contained a space (or other special shell
    characters), the shell command would fail.

--- tests/func/test_browse.sh
+++ tests/func/test_browse.sh
@@ -22,9 +22,20 @@ trap finish EXIT
 mkdir -p adir/bdir
 touch adir/bdir/hi
 
+# setup tmpdir with space in it (should be escaped)
+mkdir -p 'a sp/b sp'
+touch 'a sp/b sp/h i'
+
 # ensure that we can browse into adir, bdir, then select hi
 # ensure path is relative to where we started
 macro='C-b C-f a d i r enter enter C-f b d i r enter enter C-f h i enter enter'
 declare -A expected
 expected[hi]='^bview.[[:digit:]]+.buffer.path=adir/bdir/hi$'
+source "$this_dir/test.sh"
+
+# ensure that we can browse into 'a sp', 'b sp', then select 'h i'
+# ensure path is relative to where we started
+macro='C-b C-f a space s p enter enter C-f b space s p enter enter C-f h space i enter enter'
+declare -A expected
+expected[hi_space]='^bview.[[:digit:]]+.buffer.path=a sp/b sp/h i$'
 source "$this_dir/test.sh"
